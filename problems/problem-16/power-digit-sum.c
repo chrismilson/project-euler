731f272bbce77d3db8e2da71b9d61a5ca2825650
Chris Milson <chris.w.milson@gmail.com>
2017-05-08 23:07:49 +1200

    "Init Problem 16"

--- problems/problem-16/power-digit-sum.c
+++ problems/problem-16/power-digit-sum.c
@@ -1,9 +1,17 @@
 /* Chris Milson May 2017
 ** Problem 16
 *
-* 2^15 = 32768 and 3 + 2 + 7 + 6 + 8 = 26.
 *
-* What is the sum of the digits (in base ten) of 2^1000?
 *
 * Answer :
 */
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+	return 0;
+}
