581f6319fbf03ab5ca0dd7270965d0c11d842f3c
Chris Milson <chris.w.milson@gmail.com>
2020-04-11 10:53:30 +0900

    Solve problem 13

---
+++ problems/problem-13/large-sum.py
@@ -0,0 +1,11 @@
+""" Chris Milson April 2020
+Problem 13
+
+Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+
+*omitted*
+
+Answer : 5537376230
+"""
+from numbers import numbers
+print(str(sum(numbers))[:10])
